6db5480e14261667765e965e3be022a7be5f3347
Scott Shawcroft <scott@tannewt.org>
2023-02-08 11:58:24 -0800

    Support simple triggers

--- sigrokdecode/__init__.py
+++ sigrokdecode/__init__.py
@@ -6,7 +6,7 @@ if sys.version_info < (3, 10):
 else:
     from importlib.metadata import entry_points
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 class OutputType(Enum):
     SRD_OUTPUT_ANN = 0
--- sigrokdecode/cli.py
+++ sigrokdecode/cli.py
@@ -2,34 +2,36 @@ import click
 import pathlib
 from serial.tools import list_ports
 import sys
+
 if sys.version_info < (3, 10):
     from importlib_metadata import entry_points
 else:
     from importlib.metadata import entry_points
 
-drivers = entry_points(group='pysigrok.hardware')
+drivers = entry_points(group="pysigrok.hardware")
 driver_classes = {}
 for hw in drivers:
     loaded = hw.load()
     driver_classes[loaded.name] = loaded
 
-input_formats = entry_points(group='pysigrok.input_format')
+input_formats = entry_points(group="pysigrok.input_format")
 input_classes = {}
 for f in input_formats:
     loaded = f.load()
     input_classes[loaded.name] = loaded
 
-output_formats = entry_points(group='pysigrok.output_format')
+output_formats = entry_points(group="pysigrok.output_format")
 output_classes = {}
 for f in output_formats:
     loaded = f.load()
     output_classes[loaded.name] = loaded
 
-decoders = entry_points(group='pysigrok.decoders')
+decoders = entry_points(group="pysigrok.decoders")
 decoder_classes = {}
 for decoder in decoders:
     loaded = decoder.load()
     decoder_classes[loaded.id] = loaded
+
 
 @click.command()
 @click.option("--list-supported", "-L", is_flag=True, default=False)
@@ -40,13 +42,30 @@ for decoder in decoders:
 @click.option("-I", "--input-format", default="srzip")
 @click.option("-o", "--output-file")
 @click.option("-O", "--output-format")
-@click.option("-C", "--channels")
+@click.option("-C", "--channels", help="comma separated list of channels to capture")
+@click.option("-t", "--triggers", help="comma separated list of triggers")
+@click.option("-w", "--wait-trigger", is_flag=True, default=False)
 @click.option("--time", "sample_time")
 @click.option("--samples", type=int)
 @click.option("--frames")
 @click.option("--continuous", is_flag=True)
-def main(list_supported, list_serial, driver, configs, input_file, input_format, output_file, output_format, channels, sample_time, samples, frames, continuous):
-
+def main(
+    list_supported,
+    list_serial,
+    driver,
+    configs,
+    input_file,
+    input_format,
+    output_file,
+    output_format,
+    channels,
+    triggers,
+    wait_trigger,
+    sample_time,
+    samples,
+    frames,
+    continuous,
+):
     if list_supported:
         print("Supported hardware drivers:")
         for driver_id in driver_classes:
@@ -98,13 +117,23 @@ def main(list_supported, list_serial, driver, configs, input_file, input_format,
         driver = driver_class(channels, **driver_options, **driver_configs)
 
         if samples:
+            triggers = triggers.split(",")
+            trigger_dict = {}
+            for t in triggers:
+                pin, condition = t.split("=")
+                if len(condition) > 1 or condition not in "01rfe":
+                    raise ValueError("Only single 0, 1, r, f or e triggers supported.")
+                if condition == "0":
+                    condition = "l"
+                elif condition == "1":
+                    condition = "h"
+                trigger_dict[pin] = condition
             # acquire data
-            driver.acquire(samples)
+            driver.acquire(samples, trigger_dict, not wait_trigger)
 
     elif input_file:
         input_class = input_classes[input_format]
         driver = input_class(input_file)
-
 
     if output_file:
         # Delete the file if it exists
@@ -132,7 +161,13 @@ def main(list_supported, list_serial, driver, configs, input_file, input_format,
 
     output_class = output_classes[output_format_id]
 
-    next_decoder = output_class(f, driver, logic_channels=driver.logic_channels, analog_channels=driver.analog_channels, **output_options)
+    next_decoder = output_class(
+        f,
+        driver,
+        logic_channels=driver.logic_channels,
+        analog_channels=driver.analog_channels,
+        **output_options,
+    )
     next_decoder.reset()
     next_decoder.start()
     next_decoder.run(driver)
